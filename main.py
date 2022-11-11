6d842e130a9b105128b5ad376b074f6443afb4a1
Конопченко Леонид <popa2003kon@gmail.com>
2022-11-11 21:52:22 +0700

    finish

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import main_ui
+'''
+Модуль работы с переводом слов через google api и их записи в базу данных
+'''
+
+
 import sqlite
 
 
 def translate_action(text, origin, dest):
     db = sqlite.connect_db()
-    check = db.cursor()
-    check.execute("select * from ru;")
-    for row in check.fetchall():
-        print(row)
-    check.close()
     if origin == "ru":
         try:
             check = db.cursor()
@@ -21,7 +20,7 @@ def translate_action(text, origin, dest):
             if query_result is None:
                 from googletrans import Translator
                 translator = Translator()
-                result = translator.translate(text, src=origin, dest=dest).text
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
                 insert = db.cursor()
                 insert.execute("insert into ru ("
                                "origin_text, "
@@ -40,9 +39,6 @@ def translate_action(text, origin, dest):
             else:
                 return query_result[0]
 
-        except db.Error as error:
-            print("Ошибка чтения кэша", error)
-
         finally:
             if db:
                 db.close()
@@ -58,7 +54,7 @@ def translate_action(text, origin, dest):
             if query_result is None:
                 from googletrans import Translator
                 translator = Translator()
-                result = translator.translate(text, src=origin, dest=dest).text
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
                 insert = db.cursor()
                 insert.execute("insert into ru ("
                                "origin_text, "
@@ -77,18 +73,367 @@ def translate_action(text, origin, dest):
             else:
                 return query_result[0]
 
-        except db.Error as error:
-            print("Ошибка чтения кэша", error)
+        finally:
+            if db:
+                db.close()
+
+    elif origin == "be":
+            try:
+                check = db.cursor()
+                check.execute("select dest_text from be where origin_text = :text and dest_lan = :dest;",
+                              {"text": text,
+                               "dest": dest
+                               })
+                query_result = check.fetchone()
+                check.close()
+                if query_result is None:
+                    from googletrans import Translator
+                    translator = Translator()
+                    result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                    insert = db.cursor()
+                    insert.execute("insert into be ("
+                                   "origin_text, "
+                                   "dest_lan,"
+                                   "dest_text) values ("
+                                   "                    :origin, "
+                                   "                    :dest, "
+                                   "                    :result)",
+                                   {"origin": text,
+                                    "dest": dest,
+                                    "result": result
+                                    })
+                    db.commit()
+                    insert.close()
+                    return result
+                else:
+                    return query_result[0]
+
+
+            finally:
+                if db:
+                    db.close()
+
+    elif dest == "be":
+        try:
+            check = db.cursor()
+            check.execute("select origin_text from be where dest_text = :text and dest_lan = :origin;",
+                          {"text": text,
+                           "origin": origin
+                           })
+            query_result = check.fetchone()
+            check.close()
+            if query_result is None:
+                from googletrans import Translator
+                translator = Translator()
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                insert = db.cursor()
+                insert.execute("insert into be ("
+                               "origin_text, "
+                               "dest_lan,"
+                               "dest_text) values ("
+                               "                    :result, "
+                               "                    :dest, "
+                               "                    :origin)",
+                               {"result": result,
+                                "dest": origin,
+                                "origin": text
+                                })
+                db.commit()
+                insert.close()
+                return result
+            else:
+                return query_result[0]
+
 
         finally:
             if db:
                 db.close()
 
-    else:
-        from googletrans import Translator
-        translator = Translator()
-        result = translator.translate(text, src=origin, dest=dest).text
-        return result
+    elif origin == "uk":
+            try:
+                check = db.cursor()
+                check.execute("select dest_text from uk where origin_text = :text and dest_lan = :dest;",
+                              {"text": text,
+                               "dest": dest
+                               })
+                query_result = check.fetchone()
+                check.close()
+                if query_result is None:
+                    from googletrans import Translator
+                    translator = Translator()
+                    result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                    insert = db.cursor()
+                    insert.execute("insert into uk ("
+                                   "origin_text, "
+                                   "dest_lan,"
+                                   "dest_text) values ("
+                                   "                    :origin, "
+                                   "                    :dest, "
+                                   "                    :result)",
+                                   {"origin": text,
+                                    "dest": dest,
+                                    "result": result
+                                    })
+                    db.commit()
+                    insert.close()
+                    return result
+                else:
+                    return query_result[0]
+
+
+            finally:
+                if db:
+                    db.close()
+
+    elif dest == "uk":
+        try:
+            check = db.cursor()
+            check.execute("select origin_text from uk where dest_text = :text and dest_lan = :origin;",
+                          {"text": text,
+                           "origin": origin
+                           })
+            query_result = check.fetchone()
+            check.close()
+            if query_result is None:
+                from googletrans import Translator
+                translator = Translator()
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                insert = db.cursor()
+                insert.execute("insert into uk ("
+                               "origin_text, "
+                               "dest_lan,"
+                               "dest_text) values ("
+                               "                    :result, "
+                               "                    :dest, "
+                               "                    :origin)",
+                               {"result": result,
+                                "dest": origin,
+                                "origin": text
+                                })
+                db.commit()
+                insert.close()
+                return result
+            else:
+                return query_result[0]
+
+        finally:
+            if db:
+                db.close()
+
+    elif origin == "en":
+            try:
+                check = db.cursor()
+                check.execute("select dest_text from en where origin_text = :text and dest_lan = :dest;",
+                              {"text": text,
+                               "dest": dest
+                               })
+                query_result = check.fetchone()
+                check.close()
+                if query_result is None:
+                    from googletrans import Translator
+                    translator = Translator()
+                    result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                    insert = db.cursor()
+                    insert.execute("insert into en ("
+                                   "origin_text, "
+                                   "dest_lan,"
+                                   "dest_text) values ("
+                                   "                    :origin, "
+                                   "                    :dest, "
+                                   "                    :result)",
+                                   {"origin": text,
+                                    "dest": dest,
+                                    "result": result
+                                    })
+                    db.commit()
+                    insert.close()
+                    return result
+                else:
+                    return query_result[0]
+
+
+            finally:
+                if db:
+                    db.close()
+
+    elif dest == "en":
+        try:
+            check = db.cursor()
+            check.execute("select origin_text from en where dest_text = :text and dest_lan = :origin;",
+                          {"text": text,
+                           "origin": origin
+                           })
+            query_result = check.fetchone()
+            check.close()
+            if query_result is None:
+                from googletrans import Translator
+                translator = Translator()
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                insert = db.cursor()
+                insert.execute("insert into en ("
+                               "origin_text, "
+                               "dest_lan,"
+                               "dest_text) values ("
+                               "                    :result, "
+                               "                    :dest, "
+                               "                    :origin)",
+                               {"result": result,
+                                "dest": origin,
+                                "origin": text
+                                })
+                db.commit()
+                insert.close()
+                return result
+            else:
+                return query_result[0]
+
+
+        finally:
+            if db:
+                db.close()
+
+    elif origin == "de":
+            try:
+                check = db.cursor()
+                check.execute("select dest_text from de where origin_text = :text and dest_lan = :dest;",
+                              {"text": text,
+                               "dest": dest
+                               })
+                query_result = check.fetchone()
+                check.close()
+                if query_result is None:
+                    from googletrans import Translator
+                    translator = Translator()
+                    result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                    insert = db.cursor()
+                    insert.execute("insert into de ("
+                                   "origin_text, "
+                                   "dest_lan,"
+                                   "dest_text) values ("
+                                   "                    :origin, "
+                                   "                    :dest, "
+                                   "                    :result)",
+                                   {"origin": text,
+                                    "dest": dest,
+                                    "result": result
+                                    })
+                    db.commit()
+                    insert.close()
+                    return result
+                else:
+                    return query_result[0]
+
+
+            finally:
+                if db:
+                    db.close()
+
+    elif dest == "de":
+        try:
+            check = db.cursor()
+            check.execute("select origin_text from de where dest_text = :text and dest_lan = :origin;",
+                          {"text": text,
+                           "origin": origin
+                           })
+            query_result = check.fetchone()
+            check.close()
+            if query_result is None:
+                from googletrans import Translator
+                translator = Translator()
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                insert = db.cursor()
+                insert.execute("insert into de ("
+                               "origin_text, "
+                               "dest_lan,"
+                               "dest_text) values ("
+                               "                    :result, "
+                               "                    :dest, "
+                               "                    :origin)",
+                               {"result": result,
+                                "dest": origin,
+                                "origin": text
+                                })
+                db.commit()
+                insert.close()
+                return result
+            else:
+                return query_result[0]
+
+
+        finally:
+            if db:
+                db.close()
+
+    elif origin == "fr":
+            try:
+                check = db.cursor()
+                check.execute("select dest_text from fr where origin_text = :text and dest_lan = :dest;",
+                              {"text": text,
+                               "dest": dest
+                               })
+                query_result = check.fetchone()
+                check.close()
+                if query_result is None:
+                    from googletrans import Translator
+                    translator = Translator()
+                    result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                    insert = db.cursor()
+                    insert.execute("insert into fr ("
+                                   "origin_text, "
+                                   "dest_lan,"
+                                   "dest_text) values ("
+                                   "                    :origin, "
+                                   "                    :dest, "
+                                   "                    :result)",
+                                   {"origin": text,
+                                    "dest": dest,
+                                    "result": result
+                                    })
+                    db.commit()
+                    insert.close()
+                    return result
+                else:
+                    return query_result[0]
+
+            finally:
+                if db:
+                    db.close()
+
+    elif dest == "fr":
+        try:
+            check = db.cursor()
+            check.execute("select origin_text from fr where dest_text = :text and dest_lan = :origin;",
+                          {"text": text,
+                           "origin": origin
+                           })
+            query_result = check.fetchone()
+            check.close()
+            if query_result is None:
+                from googletrans import Translator
+                translator = Translator()
+                result = translator.translate(text, src=origin, dest=dest).text.lower().strip()
+                insert = db.cursor()
+                insert.execute("insert into fr ("
+                               "origin_text, "
+                               "dest_lan,"
+                               "dest_text) values ("
+                               "                    :result, "
+                               "                    :dest, "
+                               "                    :origin)",
+                               {"result": result,
+                                "dest": origin,
+                                "origin": text
+                                })
+                db.commit()
+                insert.close()
+                return result
+            else:
+                return query_result[0]
+
+
+        finally:
+            if db:
+                db.close()
 
 def get_word_list():
     db = sqlite.connect_db()
--- sqlite.py
+++ sqlite.py
@@ -1,4 +1,8 @@
 import sqlite3
+'''
+Модуль подключения к базе данных и создания таблиц для языков 
+
+'''
 
 def connect_db():
     global conn
@@ -9,7 +13,47 @@ def connect_db():
                                                dest_lan varchar,
                                                dest_text varchar
                                            ); 
-                             create unique index if not exists ru_uni on ru (dest_lan, dest_text);                            
+                             create unique index if not exists ru_uni on ru (dest_lan, dest_text);
+                             
+                             CREATE TABLE IF NOT EXISTS be (
+                                               id integer PRIMARY KEY NOT NULL,
+                                               origin_text varchar,
+                                               dest_lan varchar,
+                                               dest_text varchar
+                                           ); 
+                             create unique index if not exists be_uni on be (dest_lan, dest_text);
+                             
+                             CREATE TABLE IF NOT EXISTS uk (
+                                               id integer PRIMARY KEY NOT NULL,
+                                               origin_text varchar,
+                                               dest_lan varchar,
+                                               dest_text varchar
+                                           ); 
+                             create unique index if not exists uk_uni on uk (dest_lan, dest_text);   
+                             
+                             CREATE TABLE IF NOT EXISTS en (
+                                               id integer PRIMARY KEY NOT NULL,
+                                               origin_text varchar,
+                                               dest_lan varchar,
+                                               dest_text varchar
+                                           ); 
+                             create unique index if not exists en_uni on en (dest_lan, dest_text);
+                             
+                             CREATE TABLE IF NOT EXISTS de (
+                                               id integer PRIMARY KEY NOT NULL,
+                                               origin_text varchar,
+                                               dest_lan varchar,
+                                               dest_text varchar
+                                           ); 
+                             create unique index if not exists de_uni on de (dest_lan, dest_text); 
+                             
+                             CREATE TABLE IF NOT EXISTS fr (
+                                               id integer PRIMARY KEY NOT NULL,
+                                               origin_text varchar,
+                                               dest_lan varchar,
+                                               dest_text varchar
+                                           ); 
+                             create unique index if not exists fr_uni on fr (dest_lan, dest_text);                                       
                               """
         conn = sqlite3.connect('translator.db')
         c = conn.cursor()
